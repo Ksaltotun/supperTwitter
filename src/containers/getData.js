56fc9b198203424559bd732eb5e73af5b525eb3a
Ksaltotun <romah28ius@gmail.com>
2019-11-12 14:24:07 +0300

    Abaddoned all

--- src/containers/getData.js
+++
@@ -1,9 +0,0 @@
-async function getData() {
-    const posts = await fetch('http://localhost:3001/posts', {
-        method: "GET"
-    });
-    const postsArray = await posts.json();
-    return postsArray;
-}
-
-export default getData
